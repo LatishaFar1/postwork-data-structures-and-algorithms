2304d409f85c7caf843e161f0e37c42dc8c9234e
Tish Faroul <lfaroul14@gmail.com>
2022-05-03 15:02:07 -0400

    selection sort complete

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,20 +1,70 @@
+// Sort an Array of numbers using selection sort. The selection sort algorithm
+// sorts an array by repeatedly finding the minimum element (lowest value) in the
+// input Array, and then putting it at the correct location in the sorted Array.
+
+// ```js
+// Input: [3, -1, 5, 2]
+// Output: [-1, 2, 3, 5]
+// ```
+
+
 function selectionSort(arr) {
-  // type your code here
+    // type your code here
+
+    // loop through the array
+
+    for (let i = 0; i < arr.length; i++) {
+
+        // declare the variable that will hold the smallest element 
+        let lowest = i;
+
+        // inner for loop that will start from lowest + 1
+        for (let j = i + 1; j < arr.length; j++) {
+            // check if the current value is less than lowest, if it is change lowest to that value
+            if (arr[j] < arr[lowest]) {
+                lowest = j
+            }
+        }
+        if (lowest !== i) {
+            [arr[i], arr[lowest]] = [arr[lowest], arr[i]]
+        }
+
+    }
+
+    //return array
+    return arr
 }
 
+
+// ANOTHER SOLUTION:
+
+// function selectionSort(arr) {
+//   const sorted = [];
+
+//   while (arr.length > 0) {
+//     const min = Math.min(...arr);
+//     const idx = arr.indexOf(min);
+
+//     sorted.push(min);
+//     arr.splice(idx, 1);
+//   }
+
+//   return sorted;
+// }
+
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSort([3, -1, 5, 2]));
+    // add your own tests in here
+    console.log("Expecting: [-1, 2, 3, 5]");
+    console.log("=>", selectionSort([3, -1, 5, 2]));
 
-  console.log("");
+    console.log("");
 
-  // BENCHMARK HERE, and print the average runtime
-  const longInput = [];
+    // BENCHMARK HERE, and print the average runtime
+    const longInput = [];
 
-  for (let i = 0; i < 100; ++i) {
-    longInput.push(Math.random());
-  }
+    for (let i = 0; i < 100; ++i) {
+        longInput.push(Math.random());
+    }
 }
 
 module.exports = selectionSort;
